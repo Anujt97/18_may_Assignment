62df9908816a48059588085eb64287738495576a
Anujkumar Tiwari <93044865+Anujt97@users.noreply.github.com>
2022-05-22 16:45:33 +0530

    Add files via upload

---
+++ Input.js
@@ -0,0 +1,20 @@
+function Input(){
+    return(
+        <div>
+            <label>Name : </label>
+            <input type="text" placeholder="Enter your name"></input><br/><br/>
+            <label>Address : </label>
+            <input type="text" placeholder="Enter your Address"></input><br/><br/>
+            <label>email : </label>
+            <input type="email" placeholder="Enter your email"></input><br/><br/>
+            <label> Select your Skills : </label><br/>
+            <label> HTML </label>
+            <input type="checkbox" ></input><br/><br/>
+            <label> CSS </label>
+            <input type="checkbox" ></input><br/><br/>
+            <label> Javascript </label>
+            <input type="checkbox" ></input><br/><br/>
+        </div>
+        
+    )
+}
